56db7ed078b5b85700b82dab83318bae329ce2c0
RAVINDRA YADAV <36786868+ravindra281298@users.noreply.github.com>
2020-10-04 14:56:34 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     // URI : 'mongodb://localhost/urlShortner',
-    URI : 'mongodb+srv://ravindra281298:ravindra281298@devconnector-pxenm.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    URI : 'mongodb+srv://<Username>:<password>@devconnector-pxenm.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
